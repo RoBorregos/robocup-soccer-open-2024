5cbd561866003f5c0ddfec7a03e0d0ce26e4f3b5
JocelynVelarde <a01285780@itesm.mx>
2023-12-28 14:38:57 -0600

    updated constructor parameters for omni 4 drive with pwm and controllers

--- Control/Motors.cpp
+++ Control/Motors.cpp
@@ -1,30 +1,67 @@
 #include "Motors.h"
 
 
-Motors::Motors(int A1, int A2, int B1, int B2, int C1, int C2, int D1, int D2)
+Motors::Motors(int A1, int A2, int A3, int A4, int B1, int B2, int B3, int B4, int C1, int C2, int C3, int C4, int D1, int D2, int D3, int D4)
 {
     Omni4 = true;
+
+    //First set of motors
     MotorLeftF1 = A1;
     MotorLeftF2 = A2;
+    MotorLeftF_PWM = A3;
+    MotorLeftF_CNT = A4;
+
+    //Second set of motors
     MotorRightF1 = B1;
     MotorRightF2 = B2;
+    MotorRightF_PWM = B3;
+    MotorRightF_CNT = B4;
+
+    //Third set of motors
     MotorRightB1 = C1; 
     MotorRightB2 = C2;
+    MotorRightB_PWM = C3;
+    MotorRightB_CNT = C4;
+
+    //Fourth set of motors
     MotorLeftB1 = D1;
     MotorLeftB2 = D2;
+    MotorLeftB_PWM = D3;
+    MotorLeftB_CNT = D4;
 } 
 
 void Motors::InitializeMotors()
 {
     if (Omni4){
+        //Set motor pins as outputs
         pinMode(MotorLeftF1, OUTPUT);
         pinMode(MotorLeftF2, OUTPUT);
+        pinMode(MotorLeftF_PWM, OUTPUT);
+        pinMode(MotorLeftF_CNT, OUTPUT);
         pinMode(MotorRightF1, OUTPUT);
         pinMode(MotorRightF2, OUTPUT);
+        pinMode(MotorRightF_PWM, OUTPUT);
+        pinMode(MotorRightF_CNT, OUTPUT);
         pinMode(MotorRightB1, OUTPUT);
         pinMode(MotorRightB2, OUTPUT);
+        pinMode(MotorRightB_PWM, OUTPUT);
+        pinMode(MotorRightB_CNT, OUTPUT);
         pinMode(MotorLeftB1, OUTPUT);
         pinMode(MotorLeftB2, OUTPUT);
+        pinMode(MotorLeftB_PWM, OUTPUT);
+        pinMode(MotorLeftB_CNT, OUTPUT);
+
+        //Enable the motor driver
+        digitalWrite(MotorLeftF_CNT, HIGH);
+        digitalWrite(MotorRightF_CNT, HIGH);
+        digitalWrite(MotorRightB_CNT, HIGH);
+        digitalWrite(MotorLeftB_CNT, HIGH);
+
+        //Set PWM value (0 to 255)
+        analogWrite(MotorLeftF_PWM, 128); 
+        analogWrite(MotorRightF_PWM, 128);
+        analogWrite(MotorRightB_PWM, 128); 
+        analogWrite(MotorLeftB_PWM, 128); 
     }
 }
 
--- Control/Motors.h
+++ Control/Motors.h
@@ -13,15 +13,23 @@ class Motors
         bool Omni4;
         int MotorLeftF1;
         int MotorLeftF2;
+        int MotorLeftF_PWM;
+        int MotorLeftF_CNT;
         int MotorRightF1;
         int MotorRightF2;
+        int MotorRightF_PWM;
+        int MotorRightF_CNT;
         int MotorRightB1;
         int MotorRightB2;
+        int MotorRightB_PWM;
+        int MotorRightB_CNT;
         int MotorLeftB1;
         int MotorLeftB2;
+        int MotorLeftB_PWM;
+        int MotorLeftB_CNT;
         
         
-        Motors(int A1, int A2, int B1, int B2, int C1, int C2, int D1, int D2);
+        Motors(int A1, int A2, int A3, int A4, int B1, int B2, int B3, int B4, int C1, int C2, int C3, int C4, int D1, int D2, int D3, int D4);
         // Constructor for Omni4.
         void InitializeMotors();
         // Include in setup ti Initialize Motors.
