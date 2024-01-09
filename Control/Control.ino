308793381e8130fa5770140a1a4ef904b06bdc8f
JocelynVelarde <a01285780@itesm.mx>
2024-01-08 18:27:25 -0600

    finished issue with motor control, moveMotots now functioning correctly

--- Control/Control.ino
+++ Control/Control.ino
@@ -1,16 +1,18 @@
 #include "Motors.h"
 
 //check motor polarity individually for IN1 and IN2 when robot movement is set to 0 degrees. If not correct, swap IN1 and IN2
+
+//encoders: 2, 3, 18, 19
 const int motor4In1 = 9;
 const int motor4In2 = 10;
 const int motor4PWM = 5;
 const int motor4STBY = 11;
 
 //
-const int motor3In1 = 3;   
+const int motor3In1 = 52;   
 const int motor3In2 = 50;  
 const int motor3PWM = 4;   
-const int motor3STBY = 52; 
+const int motor3STBY = 8; 
 
 //
 const int motorIn1 = 24;  
@@ -20,8 +22,9 @@ const int motorSTBY = 22;
 
 const int motor2In1 = 45; 
 const int motor2In2 = 43; 
-const int motor2PWM = 12;  
+const int motor2PWM = 13;  
 const int motor2STBY = 41;
+
 
 void setup()
 {
@@ -37,12 +40,24 @@ void setup()
 
   myMotors.InitializeMotors();
   myMotors.InitializeDriver();
-  myMotors.setSpeed(motorPWM, 128);
-  myMotors.setSpeed(motor2PWM, 128);
-  myMotors.setSpeed(motor3PWM, 128);
-  myMotors.setSpeed(motor4PWM, 128);
+  myMotors.setSpeed(motorPWM, 160);
+  myMotors.setSpeed(motor2PWM, 160);
+  myMotors.setSpeed(motor3PWM, 160);
+  myMotors.setSpeed(motor4PWM, 160);
 
-  myMotors.moveMotors(0, 128);
+  myMotors.moveMotors(45, 160);
+  delay(1000);
+  myMotors.stopMotors();
+  myMotors.moveMotors(135, 160);
+  delay(1000);
+  myMotors.stopMotors();
+  myMotors.moveMotors(225, 160);
+  delay(1000);
+  myMotors.stopMotors();
+  myMotors.moveMotors(315, 160);
+  delay(1000);
+  myMotors.stopMotors();
+  
 
 }
 
