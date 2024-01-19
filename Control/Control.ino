2f434087335bfed765fd17304da1bb85e5a886a7
JocelynVelarde <a01285780@itesm.mx>
2024-01-19 14:17:35 -0600

    format fix

--- Control/Control.ino
+++ Control/Control.ino
@@ -2,17 +2,15 @@
 
 #include "Imu.h"
 Imu myImu;
-void setup(){
-    Serial.begin(115200);
-     myImu.InitializeImu();
-    
-    
-      
+void setup()
+{
+  Serial.begin(115200);
+  myImu.InitializeImu();
 }
 
-void loop(){
-   myImu.getImuData();
-   delay(100);
-    Serial.println(myImu.getYaw());
-  
+void loop()
+{
+  myImu.getImuData();
+  delay(100);
+  Serial.println(myImu.getYaw());
 }
--- Control/Imu.h
+++ Control/Imu.h
@@ -4,29 +4,29 @@
 
 #include "ICM_20948.h"
 
-
 #define WIRE_PORT Wire
 #define SERIAL_PORT Serial
 #define AD0_VAL 9
 
-class Imu{
-    private:
-        double yaw;
-        ICM_20948_I2C imu;
-        double yawOffset;
-        double pitch;
-        double roll;
+class Imu
+{
+private:
+    double yaw;
+    ICM_20948_I2C imu;
+    double yawOffset;
+    double pitch;
+    double roll;
 
-    public: 
-        Imu();
-        void InitializeImu();
-        void getImuData();
-        double getYaw();
-        void setYaw(double y);
-        void moveWithImu(double targetAngle, double speed);
-        double getPitch();
-        double getRoll();
-        void setYawOffset(double y);
+public:
+    Imu();
+    void InitializeImu();
+    void getImuData();
+    double getYaw();
+    void setYaw(double y);
+    void moveWithImu(double targetAngle, double speed);
+    double getPitch();
+    double getRoll();
+    void setYawOffset(double y);
 };
 
 #endif
