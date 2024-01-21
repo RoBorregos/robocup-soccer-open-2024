82cb47a5b022e195d0a38c9c33d005c3c081a6e6
JocelynVelarde <a01285780@itesm.mx>
2024-01-21 14:26:13 -0600

    added several scripts to read motor encoder with pulses, rpm and angular velocity

---
+++ Control/Imu.ino
@@ -0,0 +1,16 @@
+/*
+#include "Imu.h"
+Imu myImu;
+void setup()
+{
+  Serial.begin(115200);
+  myImu.InitializeImu();
+}
+
+void loop()
+{
+  myImu.getImuData();
+  delay(100);
+  Serial.println(myImu.getYaw());
+}
+*/
--- Control/RpmAndPulses.ino
+++ Control/RpmAndPulses.ino
@@ -1,73 +1,104 @@
-#include <Arduino.h>
+/*#include <Arduino.h>
 
 // Motor encoder output pulses per 360 degree revolution (measured manually)
 #define ENC_COUNT_REV 620
-
+ 
 // Encoder output to Arduino Interrupt pin. Tracks the pulse count.
 #define ENC_IN_RIGHT_A 2
-
+ 
+// Other encoder output to Arduino to keep track of wheel direction
+// Tracks the direction of rotation.
+#define ENC_IN_RIGHT_B 4
+ 
 // True = Forward; False = Reverse
 boolean Direction_right = true;
-
+ 
 // Keep track of the number of right wheel pulses
 volatile long right_wheel_pulse_count = 0;
-
+ 
 // One-second interval for measurements
 int interval = 1000;
-
+  
 // Counters for milliseconds during interval
 long previousMillis = 0;
 long currentMillis = 0;
-
-// Variables for RPM and angular velocity measurement
+ 
+// Variable for RPM measuerment
 float rpm_right = 0;
+ 
+// Variable for angular velocity measurement
 float ang_velocity_right = 0;
-
+float ang_velocity_right_deg = 0;
+ 
+const float rpm_to_radians = 0.10471975512;
+const float rad_to_deg = 57.29578;
+ 
 void setup() {
-
+ 
   // Open the serial port at 9600 bps
-  Serial.begin(9600);
-
-  // Set pin state of the encoder
-  pinMode(ENC_IN_RIGHT_A, INPUT_PULLUP);
-
+  Serial.begin(9600); 
+ 
+  // Set pin states of the encoder
+  pinMode(ENC_IN_RIGHT_A , INPUT_PULLUP);
+  pinMode(ENC_IN_RIGHT_B , INPUT);
+ 
   // Every time the pin goes high, this is a pulse
   attachInterrupt(digitalPinToInterrupt(ENC_IN_RIGHT_A), right_wheel_pulse, RISING);
-
+   
 }
-
+ 
 void loop() {
-
+ 
   // Record the time
   currentMillis = millis();
-
-  // If one second has passed, print the number of pulses and calculate RPM and angular velocity
+ 
+  // If one second has passed, print the number of pulses
   if (currentMillis - previousMillis > interval) {
-
+ 
     previousMillis = currentMillis;
-
+ 
     // Calculate revolutions per minute
     rpm_right = (float)(right_wheel_pulse_count * 60 / ENC_COUNT_REV);
-
-    // Calculate angular velocity in radians per second
-    ang_velocity_right = (rpm_right * 2 * PI) / 60;
-
+    ang_velocity_right = rpm_right * rpm_to_radians;   
+    ang_velocity_right_deg = ang_velocity_right * rad_to_deg;
+     
     Serial.print(" Pulses: ");
     Serial.println(right_wheel_pulse_count);
     Serial.print(" Speed: ");
     Serial.print(rpm_right);
     Serial.println(" RPM");
     Serial.print(" Angular Velocity: ");
-    Serial.print(ang_velocity_right);
-    Serial.println(" rad/s");
+    Serial.print(rpm_right);
+    Serial.print(" rad per second");
+    Serial.print("\t");
+    Serial.print(ang_velocity_right_deg);
+    Serial.println(" deg per second");
     Serial.println();
-
+ 
     right_wheel_pulse_count = 0;
+   
+  }
+}
+ 
+// Increment the number of pulses by 1
+void right_wheel_pulse() {
+   
+  // Read the value for the encoder for the right wheel
+  int val = digitalRead(ENC_IN_RIGHT_B);
+ 
+  if(val == LOW) {
+    Direction_right = false; // Reverse
+  }
+  else {
+    Direction_right = true; // Forward
+  }
+   
+  if (Direction_right) {
+    right_wheel_pulse_count++;
+  }
+  else {
+    right_wheel_pulse_count--;
   }
 }
 
-// Increment the number of pulses by 1
-void right_wheel_pulse() {
-  // Increment the pulse count
-  right_wheel_pulse_count++;
-}
+*/  
