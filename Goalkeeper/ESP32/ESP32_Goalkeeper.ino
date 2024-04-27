583a43323a486f9c9a0403c6f31f17f9c05c5dc9
JocelynVelarde <a01285780@itesm.mx>
2024-04-26 22:43:05 -0600

    refactored code for goalkeeper in esp32

--- Goalkeeper/ESP32/ESP32_Goalkeeper.ino
+++ Goalkeeper/ESP32/ESP32_Goalkeeper.ino
@@ -1,116 +1,90 @@
 #include <Simple_MPU6050.h>
 #include <Wire.h>
 
-
-
 #define RXD2 16
 #define TXD2 17
-const int numSamples = 10; // Número de muestras para promediar
-int samples[numSamples]; // Arreglo para almacenar las muestras
-int sampleIndex = 0; // Índice actual en el arreglo de muestras
 
-// Compass:
-Simple_MPU6050 mpu;
+const int kNumSamples = 10;
+int samples[kNumSamples];
+int sample_index = 0;
 double offset = 0;
+double filtered_angle = 0;
+double angle = 0;
+float ypr[3] = {0, 0, 0};
+float xyz[3] = {0, 0, 0};
 
-// Coeficientes del filtro Butterworth (2º orden, fc = 0.1 Hz)
-const float b0 = 0.000395;
-const float b1 = 0.000791;
-const float b2 = 0.000395;
-const float a1 = -1.972186;
-const float a2 = 0.972613;
-float distance_pixels = 0; 
-String angleString; 
+const float kFilterCoefB0 = 0.000395;
+const float kFilterCoefB1 = 0.000791;
+const float kFilterCoefB2 = 0.000395;
+const float kFilterCoefA1 = -1.972186;
+const float kFilterCoefA2 = 0.972613;
+
+float distance_pixels = 0;
 float ball_angle = 0;
 float ball_distance = 0;
 float goal_angle = 0;
 
+Simple_MPU6050 mpu;
 
-void gyroValues(int16_t *gyro, int16_t *accel, int32_t *quat) {
-    // Calcular ángulos de movimiento:
+void setup()
+{
+    Wire.begin();
+    Serial.begin(115200);
+    Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+}
+
+void loop()
+{
+    if (Serial.available())
+    {
+        ParseCamString(ball_distance, ball_angle, goal_angle, distance_pixels);
+    }
+    SendDataSerial(filtered_angle, ball_distance, ball_angle, goal_angle, distance_pixels);
+}
+
+// Function to get gyro values and process them to get the filtered angle (yaw)
+void GyroValues(int16_t *gyro, int16_t *accel, int32_t *quat)
+{
     Quaternion q;
     VectorFloat gravity;
-    float ypr[3] = {0, 0, 0};
-    float xyz[3] = {0, 0, 0};
+    ypr[3] = {0, 0, 0};
+    xyz[3] = {0, 0, 0};
     mpu.GetQuaternion(&q, quat);
     mpu.GetGravity(&gravity, &q);
     mpu.GetYawPitchRoll(ypr, &q, &gravity);
     mpu.ConvertToDegrees(ypr, xyz);
-    double angle = xyz[0];
-    double filtered_angle = filter(angle);
-    Serial.print(angle); Serial.print(" "); Serial.println(filtered_angle);
-    angleString = String(filtered_angle);
+    angle = xyz[0];
+    filtered_angle = filter(angle);
+
     if (Serial.available())
     {
-        String camString = Serial.readStringUntil('\n');
-        ball_distance = camString.toFloat();
-        ball_angle = camString.substring(camString.indexOf(' ') + 1).toFloat();
-        goal_angle = camString.substring(camString.indexOf(' ', camString.indexOf(' ') + 1) + 1, camString.lastIndexOf(' ')).toFloat();
-        distance_pixels = camString.substring(camString.lastIndexOf(' ') + 1).toFloat();
+        ParseCamString(ball_distance, ball_angle, goal_angle, distance_pixels);
     }
-    String ballDistance = String(ball_distance);
-    String ballAngle = String(ball_angle);
-    String goalAngle = String(goal_angle);
-    //String goalDistance = String(goal_distance);
-    String distancePixels = String(distance_pixels);
-    Serial2.print(angleString);
-    Serial2.print(",");
-    Serial2.print(ballDistance);
-    Serial2.print(",");
-    Serial2.print(ballAngle);
-    Serial2.print(",");
-    Serial2.print(goalAngle);
-    Serial2.print(",");
-    /*Serial2.print(goalDistance);
-    Serial2.print(",");*/
-    Serial2.print(distancePixels);
-    Serial2.println();
+
+    SendDataSerial(filtered_angle, ball_distance, ball_angle, goal_angle, distance_pixels);
 }
 
-/**
- *  Inicio de Arduino, configuraciones:
- *  - Motores
- *  - Comunicacion Serial con Camara
- *  - Giroscopio
- */
-void setup() {
-    // Calibracion Basica de Arduino:
-    Wire.begin();
-    Serial.begin(115200);
-    Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
-    // Calibracion del Giroscopio:
+// Function to parse a string from the serial port and update the ball_distance, ball_angle, goal_angle, and distance_pixels variables from the Camera
+void ParseCamString(float &ball_distance, float &ball_angle, float &goal_angle, float &distance_pixels)
+{
+    String camString = Serial.readStringUntil('\n');
+    ball_distance = camString.toFloat();
+    ball_angle = camString.substring(camString.indexOf(' ') + 1).toFloat();
+    goal_angle = camString.substring(camString.indexOf(' ', camString.indexOf(' ') + 1) + 1, camString.lastIndexOf(' ')).toFloat();
+    distance_pixels = camString.substring(camString.lastIndexOf(' ') + 1).toFloat();
 }
 
-/**
- *  En el Loop solamente se mandara a hacer las lecturas
- *  del giroscopio para actualizar los valores.
- */
-void loop() {
-  if (Serial.available())
-    {
-        String camString = Serial.readStringUntil('\n');
-        ball_distance = camString.toFloat();
-        ball_angle = camString.substring(camString.indexOf(' ') + 1).toFloat();
-        goal_angle = camString.substring(camString.indexOf(' ', camString.indexOf(' ') + 1) + 1, camString.lastIndexOf(' ')).toFloat();
-        distance_pixels = camString.substring(camString.lastIndexOf(' ') + 1).toFloat();
-    }
-    String ballDistance = String(ball_distance);
-    String ballAngle = String(ball_angle);
-    String goalAngle = String(goal_angle);
-    //String goalDistance = String(goal_distance);
-    String distancePixels = String(distance_pixels);
-    Serial2.print(angleString);
+// Function to send data over the serial port into the Raspberry Pi Pico
+void SendDataSerial(float filtered_angle, float ball_distance, float ball_angle, float goal_angle, float distance_pixels)
+{
+    Serial2.print(filtered_angle);
     Serial2.print(",");
-    Serial2.print(ballDistance);
+    Serial2.print(ball_distance);
     Serial2.print(",");
-    Serial2.print(ballAngle);
+    Serial2.print(ball_angle);
     Serial2.print(",");
-    Serial2.print(goalAngle);
+    Serial2.print(goal_angle);
     Serial2.print(",");
-    /*Serial2.print(goalDistance);
-    Serial2.print(",");*/
-    Serial2.print(distancePixels);
+    Serial2.print(distance_pixels);
     Serial2.println();
-
-    
 }
