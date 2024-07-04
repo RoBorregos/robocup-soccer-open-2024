2a32d908e1c2326fb4ba89bb632eba77360b6b40
JocelynVelarde <a01285780@itesm.mx>
2024-07-03 20:53:48 -0600

    implemented simple logic for both cameras to debug

--- main/main.ino
+++ main/main.ino
@@ -28,12 +28,14 @@ const int FRAME_HEIGHT = 104;
 const int FRAME_WIDTH = 158;
 
 Pixy2SPI_SS pixy;
+BNO055 my_bno;
 
 void setup()
 {
   Serial1.begin(115200);
   Serial.begin(9500);
   pixy.init();
+  my_bno.InitializeBNO();
 }
 
 double radiansToDegrees(double radians)
@@ -43,6 +45,35 @@ double radiansToDegrees(double radians)
 
 void loop()
 {
+
+  // ----------------- Gather data from OpenMV camera via UART ----------------- //
+   my_bno.GetBNOData();
+   angle = my_bno.GetYaw();
+   Serial.println(angle);
+  if (Serial1.available())
+  {
+    String camString = Serial1.readStringUntil('\n');
+    ball_distance = camString.toFloat();
+    ball_angle = camString.substring(camString.indexOf(' ') + 1).toFloat();
+    goal_angle = camString.substring(camString.indexOf(' ', camString.indexOf(' ') + 1) + 1, camString.lastIndexOf(' ')).toFloat();
+    distance_pixels = camString.substring(camString.lastIndexOf(' ') + 1).toFloat();
+    distance_pixels_protect = camString.substring(camString.indexOf(' ', camString.lastIndexOf(' ') + 1)).toFloat();
+  }
+  String angleString = String(angle);
+  String ballDistance = String(ball_distance);
+  String ballAngle = String(ball_angle);
+  String goalAngle = String(goal_angle);
+  String distancePixels = String(distance_pixels);
+  Serial.print(angleString);
+  Serial.print(",");
+  Serial.print(ballDistance);
+  Serial.print(",");
+  Serial.print(ballAngle);
+  Serial.print(",");
+  Serial.print(goalAngle);
+  Serial.print(",");
+  Serial.print(distancePixels);
+  Serial.println();
 
   // ----------------- Gather data from Pixy2 camera via SPI ----------------- //
 
@@ -55,20 +86,16 @@ void loop()
     for (i = 0; i < pixy.ccc.numBlocks; i++)
     {
       double cx = pixy.ccc.blocks[i].m_x;
-      double cy = pixy.ccc.blocks[i].m_y;
+      double cy = pixy.ccc.blocks[i].m_y; 
       double relative_cx = cx - FRAME_WIDTH;
       double relative_cy = cy - FRAME_HEIGHT;
       double magnitude_distance = sqrt(relative_cx * relative_cx + relative_cy * relative_cy);
       double total_distance = 1.2415 * magnitude_distance - 5.2805;
 
-      Serial.print("Total distance: ");
-      Serial.println(total_distance);
+
       double angle_radians = atan2(relative_cy, relative_cx);
       double angle_degrees = radiansToDegrees(angle_radians);
-
-      Serial.print("Angle: ");
-      Serial.println(angle_degrees);
-
+ 
       angle_degrees += 8;
       if (angle_degrees < 0)
       {
@@ -78,7 +105,15 @@ void loop()
       {
         angle_degrees -= 360;
       }
+
+      Serial.print("Block ");
+      Serial.print(i);
+      Serial.print(": Distance = ");
+      Serial.print(total_distance);
+      Serial.print(" cm, Angle = ");
+      Serial.print(angle_degrees);
+      Serial.println(" degrees");
+
     }
   }
 }
-
