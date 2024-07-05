6e4c3296a0e887e3201987649ff91107a52b9269
JocelynVelarde <a01285780@itesm.mx>
2024-07-05 11:42:47 -0600

    move to pixy angle when ball detected and activate dribbler

--- main/main.ino
+++ main/main.ino
@@ -5,6 +5,7 @@
 #include "PID.h"
 #include <Pixy2SPI_SS.h>
 #include "Motors.h"
+#include <Servo.h>
 
 /*
 Pixy camera resolution: 316 x 208
@@ -12,26 +13,11 @@ pixy.ccc.blocks[i].m_x The x location of the center of the detected object (0 to
 pixy.ccc.blocks[i].m_y The y location of the center of the detected object (0 to 208)
 */
 
-/*
-Pixy cam con la que veo de default puedo ver la distancia y el angulo de todas mis cosas, hay un rango en donde ya no vi mi pelota. Entonces se debe
-de tener tres condiciones de cuando no veo la pelota checar si la puedo ver en la openmv, de cuando la veo en las dos y de cuando solo la veo en la pixy. En
-base a eso puedo acercarme a la pelota en su ball angle y cuando ya la tenga enfrente (osea que la vea con la camara frontal openmv a cierta distancia y angulo)
- y si esta ahi prendo dribbler y avanzo hacia el angulo de la pelota de acuerdo a mi openmv en caso de no ver la pelota
-en ningun lado, me dirigo al angulo donde la vi por ultima vez
-*/
-#include <Servo.h>
-
-// Create a Servo object to control the ESC
-Servo esc;
-
-// Define the pin for the ESC signal wire
-const int escPin = 6;
-
-// Define speed levels
-const int minSpeed = 1000; // Minimum speed (1000 microseconds)
-const int midSpeed = 1500; // Mid speed (1500 microseconds)
-const int maxSpeed = 2000; // Maximum speed (2000 microseconds)
-const int delayTime = 3000; // Delay time in milliseconds
+const int esc_pin = 6;
+const int min_speed = 1000; 
+const int mid_speed = 1500; 
+const int max_speed = 2000;
+const int delay_time = 3000;
 float bno_angle = 0;
 bool ball_seen_openmv = false;
 double target_angle = 0;
@@ -43,19 +29,22 @@ float ball_distance = 0;
 float goal_angle = 0;
 bool ball_found = false;
 double ponderated_angle = 0;
+double angle_degrees = 0; 
 float goal_distance = 0;
 double last_time = 0;
 double current_time = 0;
-float angle = 0;
+int goal_threshold = 15;
 const int FRAME_HEIGHT = 104;
 const int FRAME_WIDTH = 158;
 const int FRAME_ROBOT = 20;
-int goal_threshold = 15;
 const int FRAME_CIRCLE = 150;
 
 Pixy2SPI_SS pixy;
 BNO055 my_bno;
+Servo esc;
+
 PID pid_w(0.6, 0.01, 6, 200);
+//PID pixy_w(1, 0, 0, 200);
 Motors myMotors(
     MOTOR1_PWM, MOTOR1_IN1, MOTOR1_IN2,
     MOTOR2_PWM, MOTOR2_IN1, MOTOR2_IN2,
@@ -64,13 +53,13 @@ Motors myMotors(
 
 void setup()
 {
-  esc.attach(escPin);
+  esc.attach(esc_pin);
   Serial1.begin(115200);
   Serial.begin(9500);
-  esc.writeMicroseconds(minSpeed);
+  esc.writeMicroseconds(min_speed);
   pixy.init();
   my_bno.InitializeBNO();
-  delay(3000);
+  delay(delay_time);
 }
 
 double radiansToDegrees(double radians)
@@ -93,10 +82,6 @@ void loop()
     distance_pixels = camString.substring(camString.lastIndexOf(' ') + 1).toFloat();
     ball_seen_openmv = (ball_distance != 0 || ball_angle != 0);
   }
-  /*Serial.print(ball_distance);
-  Serial.print(",");
-  Serial.print(ball_angle);
-  Serial.println();*/
 
   // ----------------- Gather data from Pixy2 camera via SPI ----------------- //
 
@@ -107,20 +92,19 @@ void loop()
 
   if (pixy.ccc.numBlocks)
   {
-    //Serial.println(pixy.ccc.numBlocks);
     for (i = 0; i < pixy.ccc.numBlocks; i++)
     {
       double cx = pixy.ccc.blocks[i].m_x;
       double cy = pixy.ccc.blocks[i].m_y;
-      double relative_cx = cx - FRAME_WIDTH;
-      double relative_cy = cy - FRAME_HEIGHT;
+      double relative_cx = cx - 158;
+      double relative_cy = cy - 208;
       double magnitude_distance = sqrt(relative_cx * relative_cx + relative_cy * relative_cy);
       double total_distance = 1.2415 * magnitude_distance - 5.2805;
 
       double angle_radians = atan2(relative_cy, relative_cx);
-      double angle_degrees = radiansToDegrees(angle_radians);
+      angle_degrees = radiansToDegrees(angle_radians);
 
-      angle_degrees += 70;
+      angle_degrees += 95;
       if (angle_degrees < 0)
       {
         angle_degrees += 360;
@@ -130,13 +114,8 @@ void loop()
         angle_degrees -= 360;
       }
 
-      /*Serial.print("Block ");
-      Serial.print(i);
-      Serial.print(": Distance = ");
-      Serial.print(total_distance);
-      Serial.print(" cm, Angle = ");
-      Serial.print(angle_degrees);
-      Serial.println(" degrees");*/
+      Serial.print("ANGLE PIXY: ");
+      Serial.println(angle_degrees);
 
       if (pixy.ccc.blocks[i].m_signature == 1)
       {
@@ -150,11 +129,14 @@ void loop()
       }
     }
   }
-  //Serial.println(ball_seen_pixy);
+
+  //----------------------- Calculate PIDS-----------------------------//
 
   double speed_w = pid_w.Calculate(target_angle, bno_angle);
+  //double angle_pixy = pixy_w.Calculate(target_angle, angle_degrees);
   double speed_t_goal = 150;
   double speed_t_ball = 150;
+
   if (speed_w != 0)
   {
     if (ball_angle < 180)
@@ -170,32 +152,22 @@ void loop()
     //------------------Both camera detection cases ------------------//
     if (ball_seen_pixy && ball_seen_openmv)
     {
-      myMotors.MoveMotorsImu(ball_angle, speed_t_ball, speed_w);
+      myMotors.MoveMotorsImu(angle_degrees, speed_t_ball, speed_w);
       Serial.println("Both cameras see the ball");
-      Serial.print("BALL ANGLE: ");
-      Serial.println(ball_angle);
-      //esc.writeMicroseconds(midSpeed);
+      esc.writeMicroseconds(mid_speed);
     }
     else if (ball_seen_pixy && !ball_seen_openmv)
     {
-       myMotors.MoveMotorsImu(0, speed_t_ball, speed_w);
-      
+      myMotors.MoveMotorsImu(angle_degrees, speed_t_ball, speed_w);
       Serial.println("Only pixy sees the ball");
+      esc.writeMicroseconds(mid_speed);
     }
     else if (!ball_seen_pixy && ball_seen_openmv)
     {
-      Serial.println("PELOTA ANGLE");
-
       double differential = ball_angle_180 * 0.15;
       ponderated_angle = ball_angle + differential;
-      Serial.print("PONDERATED: ");
-      Serial.println(ponderated_angle);
       myMotors.MoveMotorsImu(ponderated_angle, abs(speed_t_ball), speed_w);
-      //myMotors.MoveMotorsImu(ball_angle, abs(speed_t_ball), speed_w);
-      Serial.print("BALL ANGLE: ");
-      Serial.println(ball_angle);
-      
-      //esc.writeMicroseconds(midSpeed);
+      Serial.println("Only OpenMV sees the ball");
     }else if (goal_angle > 0)
                 {
                     if (goal_angle < (185 - goal_threshold) && ball_found == false)
@@ -213,7 +185,7 @@ void loop()
                 }
     else
     {
-      Serial.println("No camera sees the ball, move to last known position");
+      Serial.println("No camera sees the ball");
       myMotors.StopMotors();
     }
   }
